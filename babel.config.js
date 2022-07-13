1bcd61dfa19606ea4c4abc219c3d52f673597908
Olga Kazakova <olegenok@bk.ru>
2022-07-13 12:40:07 +0300

    add babel config

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = (api) => {
+  return {
+    // plugins: [
+    //   "@babel/plugin-proposal-nullish-coalescing-operator",
+    //   "@babel/plugin-proposal-optional-chaining",
+    // ],
+    presets: [
+      [
+        // "@babel/preset-typescript",
+        "@babel/preset-env",
+        {
+          useBuiltIns: "entry",
+          targets: api.caller((caller) => caller && caller.target === "node")
+            ? { node: "current" }
+            : { chrome: "58", ie: "11" },
+        },
+        "@babel/preset-typescript",
+      ],
+    ],
+  };
+};
